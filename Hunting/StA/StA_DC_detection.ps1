3ea010f85c0f7789b7066284b7cc8e3318c8e7ab
Elli <36914463+eshlomo1@users.noreply.github.com>
2021-12-22 10:16:23 +0200

    Create StA_DC_detection.ps1

---
+++ Hunting/StA/StA_DC_detection.ps1
@@ -0,0 +1,33 @@
+<#
+    Sam The Admin - Find any computer accounts that have a invalid SamAccountName 
+    To run remotely use the combination of Get-WinEvent
+    November Update - CVE-2021-42287 and CVE-2021-42278  
+    More information about the Sam the Admin
+    https://exploit.ph/cve-2021-42287-cve-2021-42278-weaponisation.html
+#>
+$EventIds = @{
+    35    = "PAC without attributes"
+    36    = "Ticket without a PAC"
+    37    = "Ticket without Requestor"
+    38    = "Requestor Mismatch"
+    4662  = "operation was performed on an object"
+    4741  = "computer account was created"
+    4781  = "account was changed"
+    4738  = "user account was changed"
+    16990 = "Object class and UserAccountControl validation failure"
+    16991 = "SAM Account Name validation failure"
+}
+#
+$DomainController = Get-ADDomain | Select-Object -ExpandProperty ReplicaDirectoryServers
+foreach ($ComputerName in $DomainController) {
+    $Events = Invoke-Command -ComputerName $ComputerName -ScriptBlock { param([string[]]$EventIds) $EventIds | Out-Null ; Get-WinEvent -EA 0 -FilterHashtable @{LogName = 'System'; id = $EventIds } | Where-Object { $_.ProviderName -in @('Microsoft-Windows-Kerberos-Key-Distribution-Center', 'Microsoft-Windows-Directory-Services-SAM') } } -ArgumentList (, $EventIds.Keys)
+
+    foreach ($Event in $Events) {
+        [PSCustomObject]@{
+            TimeCreated = $Event.TimeCreated
+            Id          = $Event.Id
+            EventGroup  = $EventIds[$Event.Id]
+            Reason      = $Event.Message
+        }
+    }
+}
